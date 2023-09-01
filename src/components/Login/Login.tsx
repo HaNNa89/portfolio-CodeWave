10f9ade6f6fffb51c782fbeaf9885644f01884e7
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-09-01 16:21:22 +0200

    layout loginform (no logic for now, WIP)

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import CodeWave from "../../assets/codewave.png";
+import "./Login.css";
+import { NavLink } from "react-router-dom";
+
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameChange = (event: any) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event: any) => {
+    setPassword(event.target.value);
+  };
+
+  return (
+    <div className="login-container">
+      <img className="logo" src={CodeWave} alt="Logotyp" />
+
+      <div className="login-form">
+        <div className={`input-container ${username ? "active" : ""}`}>
+          <input
+            type="text"
+            value={username}
+            onChange={handleUsernameChange}
+            required
+          />
+          <label>Användarnamn</label>
+        </div>
+        <div className={`input-container ${password ? "active" : ""}`}>
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+          <label>Lösenord</label>
+        </div>
+        <div className="create-account">
+          <NavLink to="/register">Skapa ett konto</NavLink>
+        </div>
+
+        <button className="login-button">Sign in</button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
