bbc2bc8031d84185100f131c2eaf871be536a177
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-09-01 16:16:08 +0200

    remove imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import { Box } from "@mui/system";
 import { Outlet } from "react-router-dom";
 import "./App.css";
-import Clients from "./Clients";
 import Footer from "./Footer";
 import Header from "./Header";
-import Hero from "./Hero";
-import ProfileCard from "./components/ProfileCard";
 
 function App() {
   return (
