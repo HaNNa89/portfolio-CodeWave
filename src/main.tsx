9e52755137900a37bfd6fefa07c0e273c0fa843b
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-09-01 16:12:05 +0200

    set up routing for homepage, login & register

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import App from "./App";
+import Homepage from "./Homepage";
+import Register from "./Register";
+import Login from "./components/Login/Login";
 import "./index.css";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index element={<Homepage />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
