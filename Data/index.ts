92ae3f74fa0e352eb918c7f42ba0f296473be34c
MadeleineGustafson <madeleinegu@gmail.com>
2023-09-01 17:35:29 +0200

    detailpage done

---
+++ Data/index.ts
@@ -0,0 +1,32 @@
+export const consults = [
+  {
+    name: "Felicia Willnäs",
+    description:
+      "Web Development Enthusiast with a Focus on Responsive Design! Felicia is a passionate web developer with a strong affinity for responsive design. Creating websites that look equally fantastic on desktops, tablets, and mobiles is my specialty",
+    imgurl: "",
+  },
+  {
+    name: "Hanna Petersson",
+
+    description:
+      "Front-End Guru Specializing in React.js! Hanna, an experienced Front-End developer with in-depth knowledge of React.js. I've worked on complex web applications and user interface projects, using React.js to create dynamic and interactive user experiences.",
+
+    imgurl: "",
+  },
+  {
+    name: "Mergim Shala",
+
+    description:
+      "UX-Focused Front-End Designer! Mergim is a Front-End Designer who is dedicated to user experience (UX) and usability. He place users' needs and behaviors at the core of my work, always striving to create designs that are not only beautiful but also functional",
+
+    imgurl: "",
+  },
+
+  {
+    name: "Madeleine Gustafsson",
+    description:
+      "Front-End Developer with E-commerce Experience! Madeleine is a Front-End Developer specializing in e-commerce. She have worked on several e-commerce projects and have deep experience in integrating payment solutions, shopping carts, and product interfaces.",
+
+    imgurl: "",
+  },
+];
---
+++ src/components/InformationPage.tsx
@@ -0,0 +1,15 @@
+import Footer from "../Footer";
+import Header from "../Header";
+import DetailCard from "./DetailCard";
+
+function InformationPage() {
+  return (
+    <>
+      <Header />
+      <DetailCard />
+      <Footer />
+    </>
+  );
+}
+
+export default InformationPage;
