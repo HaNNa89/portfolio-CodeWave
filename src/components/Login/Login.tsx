1be6f995d1b68f4071e3cb598a08536169c816e6
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-09-02 18:07:14 +0200

    validation for inputs for username & password

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,64 +1,82 @@
-import { useState, useEffect } from "react";
+import { TextField } from "@mui/material";
+import { NavLink, useNavigate } from "react-router-dom";
 import CodeWave from "../../assets/codewave.png";
 import "./Login.css";
-import { NavLink, useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+const validationSchema = yup.object({
+  username: yup.string().required("Användarnamn är obligatoriskt."),
+  password: yup
+    .string()
+    .required("Lösenord är obligatoriskt")
+    .matches(
+      /^[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]+$/,
+      "Lösenordet innehåller ogiltiga tecken."
+    )
+    .min(10, "Lösenordet behöver vara minst 10 tecken långt."),
+});
 
 function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  const handleUsernameChange = (event: any) => {
-    setUsername(event.target.value);
-  };
-
-  const handlePasswordChange = (event: any) => {
-    setPassword(event.target.value);
-  };
-
-  const handleSignInClick = () => {
-      localStorage.setItem("username", username);
-      console.log(`User ${username} has signed in!.`);
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      localStorage.setItem("username", values.username);
+      console.log(`User ${values.username} has signed in!`);
       navigate("/");
-  };
+    },
+  });
 
-  useEffect(() => {
-    const storedUsername = localStorage.getItem("username");
-    if (storedUsername) {
-      setUsername(storedUsername);
-    }
-  }, []);
 
   return (
     <div className="login-container">
       <img className="logo" src={CodeWave} alt="Logotyp" />
 
       <div className="login-form">
-        <div className={`input-container ${username ? "active" : ""}`}>
-          <input
-            type="text"
-            value={username}
-            onChange={handleUsernameChange}
-            required
-          />
-          <label>Användarnamn</label>
-        </div>
-        <div className={`input-container ${password ? "active" : ""}`}>
-          <input
-            type="password"
-            value={password}
-            onChange={handlePasswordChange}
-            required
-          />
-          <label>Lösenord</label>
-        </div>
-        <div className="create-account">
-          <NavLink to="/register">Skapa ett konto</NavLink>
-        </div>
-
-        <button className="login-button" onClick={handleSignInClick}>
-          Sign in
-        </button>
+        <form onSubmit={formik.handleSubmit}>
+          <div className={`input-container ${formik.touched.username && formik.errors.username ? "error" : ""}`}>
+            <TextField
+              type="text"
+              label="Användarnamn"
+              name="username"
+              value={formik.values.username}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.username && Boolean(formik.errors.username)}
+              helperText={(formik.touched.username && formik.errors.username) && formik.errors.username}
+              required
+              fullWidth
+            />
+          </div>
+          <div className={`input-container ${formik.touched.password && formik.errors.password  ? "error" : ""}`}>
+            <TextField
+              type="password"
+              label="Lösenord"
+              name="password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.password && Boolean(formik.errors.password)}
+              helperText={(formik.touched.password && formik.errors.password) && formik.errors.password}
+              required
+              fullWidth
+            />
+          </div>
+          <div className="create-account">
+            <NavLink style={{ color: "#000" }} to="/register">
+              Skapa ett konto
+            </NavLink>
+          </div>
+          <button className="login-button" type="button">
+            Sign in
+          </button>
+        </form>
       </div>
     </div>
   );
