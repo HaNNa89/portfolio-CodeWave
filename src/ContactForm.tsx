1162fd895f1d0b86f14677d036c9b96acbbb526c
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-09-05 14:57:53 +0200

    validation with yup & formik for fields

--- src/ContactForm.tsx
+++ src/ContactForm.tsx
@@ -1,9 +1,8 @@
-import { Box, Button, InputLabel, TextField } from "@mui/material";
+import { Button, InputLabel, TextField } from "@mui/material";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 
 export default function ContactForm() {
-
   const validationSchema = Yup.object().shape({
     name: Yup.string().required("Please enter your name."),
     email: Yup.string().email("Invalid email format").required("Please enter your email."),
@@ -21,52 +20,85 @@ export default function ContactForm() {
         console.log(values);
     },
   });
-  
+
   return (
-    <>
-      <Box style={fromContainer}>
-        <h5 style={ContactTitle}>Contact me</h5>
-        <InputLabel sx={inputLabel}>Name:</InputLabel>
-        <TextField
-          InputProps={{
-            style: {
-              borderRadius: "5rem",
-            },
-          }}
-          id="outlined-basic"
-          variant="outlined"
-        />
-        <InputLabel sx={inputLabel}>Email*:</InputLabel>
-        <TextField
-          InputProps={{
-            style: {
-              borderRadius: "5rem",
-            },
-          }}
-          id="outlined-basic"
-          variant="outlined"
-        />
-        <InputLabel sx={inputLabel}>Your message:*</InputLabel>
-        <TextField
-          InputProps={{
-            style: {
-              borderRadius: "2rem",
-            },
-          }}
-          multiline
-          rows={4}
-          id="outlined-basic"
-          variant="outlined"
-        />
-        <Button sx={buttonStyle} disableRipple variant="text">
-          Send
-        </Button>
-      </Box>
-    </>
+    <form style={formContainer} onSubmit={formik.handleSubmit}>
+      <h5 style={ContactTitle}>Contact me</h5>
+
+      <InputLabel sx={inputLabel}>Name:</InputLabel>
+      <TextField
+        InputProps={{
+          style: {
+            borderRadius: "5rem",
+          },
+        }}
+        id="name"
+        name="name"
+        variant="outlined"
+        value={formik.values.name}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+      />
+      {formik.touched.name && formik.errors.name ? (
+        <div style={errorStyle}>{formik.errors.name}</div>
+      ) : null}
+
+      <InputLabel sx={inputLabel}>Email*:</InputLabel>
+      <TextField
+        InputProps={{
+          style: {
+            borderRadius: "5rem",
+          },
+        }}
+        id="email"
+        name="email"
+        variant="outlined"
+        value={formik.values.email}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+      />
+      {formik.touched.email && formik.errors.email ? (
+        <div style={errorStyle}>{formik.errors.email}</div>
+      ) : null}
+
+      <InputLabel sx={inputLabel}>Your message:*</InputLabel>
+      <TextField
+        InputProps={{
+          style: {
+            borderRadius: "2rem",
+          },
+        }}
+        multiline
+        rows={4}
+        id="message"
+        name="message"
+        variant="outlined"
+        value={formik.values.message}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+      />
+      {formik.touched.message && formik.errors.message ? (
+        <div style={errorStyle}>{formik.errors.message}</div>
+      ) : null}
+
+      <Button
+        type="submit"
+        sx={buttonStyle}
+        onClick={() => {
+          formik.setTouched({
+            name: true,
+            email: true,
+            message: true,
+          });
+        }}
+      >
+        Send
+      </Button>
+    </form>
   );
 }
 
-const fromContainer = {
+const formContainer = {
   width: "60%",
   display: "grid",
   height: "36rem",
@@ -86,7 +118,6 @@ const ContactTitle = {
 const inputLabel = {
   fontFamily: "Montserrat",
   marginLeft: "0.5rem",
-  // marginTop: "1rem",
 };
 
 const buttonStyle = {
@@ -95,10 +126,13 @@ const buttonStyle = {
   width: "7rem",
   textTransform: "none",
   color: "#FBF9F3",
-  // marginTop: "1rem",
   boxShadow: "none",
   fontFamily: "Lexend giga",
   "&:hover": {
-    backgroundColor: "#1b1b1b ",
+    backgroundColor: "#1b1b1b",
   },
+};
+
+const errorStyle = {
+  color: "red",
 };
