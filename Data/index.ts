addafb499361b5e0420976cf2d3691b1f1104be7
HaNNa89 <petersson.h@hotmail.com>
2023-09-06 12:46:14 +0200

    add + change images

--- Data/index.ts
+++ Data/index.ts
@@ -5,7 +5,7 @@ export const team = [
       "Web Development Enthusiast with a Focus on Responsive Design",
     description:
       "Felicia is a passionate web developer with a strong affinity for responsive design. Creating websites that look equally fantastic on desktops, tablets, and mobiles is my specialty",
-    imgurl: "/pictures/girl.jpg",
+    imgurl: "/pictures/person1.jpg",
   },
   {
     name: "Hanna Petersson",
@@ -13,8 +13,7 @@ export const team = [
 
     description:
       "Hanna, an experienced Front-End developer with in-depth knowledge of React.js. Hanna has worked on complex web applications and user interface projects, using React.js to create dynamic and interactive user experiences.",
-
-    imgurl: "/pictures/girl.jpg",
+    imgurl: "/pictures/person2.jpg",
   },
   {
     name: "Mergim Shala",
@@ -22,8 +21,7 @@ export const team = [
 
     description:
       "Mergim is a Front-End Designer who is dedicated to user experience (UX) and usability. He place users' needs and behaviors at the core of my work, always striving to create designs that are not only beautiful but also functional",
-
-    imgurl: "/pictures/girl.jpg",
+    imgurl: "/pictures/person3.jpg",
   },
 
   {
@@ -31,7 +29,6 @@ export const team = [
     littledescription: "Front-End Developer with E-commerce Experience",
     description:
       "Madeleine is a Front-End Developer specializing in e-commerce. She have worked on several e-commerce projects and have deep experience in integrating payment solutions, shopping carts, and product interfaces.",
-
-    imgurl: "/pictures/girl.jpg",
+    imgurl: "/pictures/person4.jpg",
   },
 ];
