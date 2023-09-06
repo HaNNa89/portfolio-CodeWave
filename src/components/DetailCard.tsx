0e463e5beaf6c6bd2e8e3fd0056bd3c2e37183d8
MadeleineGustafson <madeleinegu@gmail.com>
2023-09-06 16:29:53 +0200

    logo and small fixes

--- src/components/DetailCard.tsx
+++ src/components/DetailCard.tsx
@@ -9,168 +9,183 @@ import { team } from "../../data/index";
 import ContactForm from "../ContactForm";
 
 function DetailCard() {
-	interface Consultant {
-		name: string;
-		description: string;
-	}
+  interface Consultant {
+    name: string;
+    description: string;
+  }
 
-	const { name } = useParams();
+  const { name } = useParams();
 
-	const consultant = team.find(
-		(consultant: Consultant) => consultant.name === name
-	);
+  const consultant = team.find(
+    (consultant: Consultant) => consultant.name === name
+  );
 
-	if (!consultant) {
-		return <div>Consultant not found</div>;
-	}
+  if (!consultant) {
+    return <div>Consultant not found</div>;
+  }
 
-	return (
-		<div>
-			<Helmet>
-				<title>Detail - Codewave</title>
-			</Helmet>
-			<Box
-				sx={{
-					width: "100%",
-					display: "flex",
-					justifyContent: "space-evenly",
-					paddingTop: "2rem",
-					fontFamily: "Lexend Giga",
-					textDecoration: "none",
-					alignItems: "center",
-				}}
-			>
-				<Link
-					to="/"
-					style={{
-						textDecoration: "none",
-						color: "inherit",
-						display: "flex",
-						alignItems: "center",
-					}}
-				>
-					<IoIosArrowRoundBack size={30} /> Back to homepage
-				</Link>
-			</Box>
-			<Grid
-				container
-				spacing={5}
-				key={consultant.name}
-				style={{ marginBottom: "2rem" }}
-			>
-				<Grid
-					item
-					lg={6}
-					md={12}
-					sm={12}
-					xs={12}
-					style={{
-						marginTop: "5rem",
-						display: "flex",
-						marginBottom: "2rem",
-					}}
-					sx={{
-						display: "flex",
-						justifyContent: "flex-start",
-						alignItems: {
-							xs: "center",
-							sm: "center",
-							md: "center",
-							lg: "flex-end",
-						},
-						flexDirection: "column",
-					}}
-				>
-					<img src={consultant.imgurl} alt="img" style={image} />
-				</Grid>
-				<Grid
-					item
-					lg={6}
-					md={12}
-					sm={12}
-					xs={12}
-					sx={{
-						display: "flex",
-						justifyContent: "center",
-						alignItems: {
-							xs: "center",
-							sm: "center",
-							md: "center",
-							lg: "flex-start",
-						},
-						marginTop: {
-							lg: "5rem",
-							md: "0rem",
-							sm: "0rem",
-						},
-						width: "100%",
-						flexDirection: "column",
-					}}
-				>
-					<Typography
-						style={{
-							fontFamily: "Lexend giga",
-							fontSize: "1.5rem",
-							fontWeight: "bold",
-							marginBottom: "0.5rem",
-						}}
-					>
-						{consultant.name}
-					</Typography>
-					<Typography
-						sx={{
-							maxWidth: { xs: "19rem", sm: "28rem", md: "28rem" },
-							fontFamily: "montserrat",
-							fontSize: "1.2rem",
-							marginBottom: "1.5rem",
-						}}
-					>
-						{consultant.description}
-					</Typography>
-					<Typography
-						style={{
-							fontFamily: "Lexend giga",
-							fontSize: "1.2rem",
-							fontWeight: "bold",
-							marginBottom: "0.5rem",
-						}}
-					>
-						KNOWLEDGE:
-					</Typography>
-					<Box style={{ display: "flex" }}>
-						<AiFillHtml5 size={40} /> <FaCss3Alt size={40} />
-						<BiLogoTypescript size={40} />
-						<BiLogoJavascript size={40} />
-						<FaReact size={40} />
-						<FaFigma size={40} />
-					</Box>
-					<Box
-						sx={{
-							width: "100%",
-							height: "auto",
-							display: "flex",
-							justifyContent: {
-								lg: "flex-start",
-								md: "center",
-								sm: "center",
-								xs: "center",
-							},
-						}}
-					>
-						<ContactForm />
-					</Box>
-				</Grid>
-			</Grid>
-		</div>
-	);
+  return (
+    <div>
+      <Helmet>
+        <title>Team - Codewave {consultant.name}</title>
+      </Helmet>
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          flexDirection: {
+            xs: "column",
+            sm: "column",
+            md: "row",
+            lg: "row",
+          },
+          justifyContent: "space-between",
+          paddingTop: "1rem",
+
+          fontFamily: "Lexend Giga",
+          textDecoration: "none",
+          alignItems: "center",
+        }}
+      >
+        <Link to="/">
+          <img
+            src="/src/assets/codewave.png"
+            style={{ width: "5.3rem", height: "auto", padding: "1rem" }}
+            alt="logotyp"
+          />
+        </Link>
+        <Link
+          to="/"
+          style={{
+            textDecoration: "none",
+            color: "inherit",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <IoIosArrowRoundBack size={30} /> Back to homepage
+        </Link>
+        <Box sx={{ width: "10rem" }}></Box>
+      </Box>
+      <Grid
+        container
+        spacing={5}
+        key={consultant.name}
+        style={{ marginBottom: "2rem" }}
+      >
+        <Grid
+          item
+          lg={6}
+          md={12}
+          sm={12}
+          xs={12}
+          style={{
+            marginTop: "5rem",
+            display: "flex",
+            marginBottom: "2rem",
+          }}
+          sx={{
+            display: "flex",
+            justifyContent: "flex-start",
+            alignItems: {
+              xs: "center",
+              sm: "center",
+              md: "center",
+              lg: "flex-end",
+            },
+            flexDirection: "column",
+          }}
+        >
+          <img src={consultant.imgurl} alt="img" style={image} />
+        </Grid>
+        <Grid
+          item
+          lg={6}
+          md={12}
+          sm={12}
+          xs={12}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: {
+              xs: "center",
+              sm: "center",
+              md: "center",
+              lg: "flex-start",
+            },
+            marginTop: {
+              lg: "5rem",
+              md: "0rem",
+              sm: "0rem",
+            },
+            width: "100%",
+            flexDirection: "column",
+          }}
+        >
+          <Typography
+            style={{
+              fontFamily: "Lexend giga",
+              fontSize: "1.5rem",
+              fontWeight: "bold",
+              marginBottom: "0.5rem",
+            }}
+          >
+            {consultant.name}
+          </Typography>
+          <Typography
+            sx={{
+              maxWidth: { xs: "19rem", sm: "28rem", md: "28rem" },
+              fontFamily: "montserrat",
+              fontSize: "1.2rem",
+              marginBottom: "1.5rem",
+            }}
+          >
+            {consultant.description}
+          </Typography>
+          <Typography
+            style={{
+              fontFamily: "Lexend giga",
+              fontSize: "1.2rem",
+              fontWeight: "bold",
+              marginBottom: "0.5rem",
+            }}
+          >
+            KNOWLEDGE:
+          </Typography>
+          <Box style={{ display: "flex" }}>
+            <AiFillHtml5 size={40} /> <FaCss3Alt size={40} />
+            <BiLogoTypescript size={40} />
+            <BiLogoJavascript size={40} />
+            <FaReact size={40} />
+            <FaFigma size={40} />
+          </Box>
+          <Box
+            sx={{
+              width: "100%",
+              height: "auto",
+              display: "flex",
+              justifyContent: {
+                lg: "flex-start",
+                md: "center",
+                sm: "center",
+                xs: "center",
+              },
+            }}
+          >
+            <ContactForm />
+          </Box>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 const image = {
-	maxWidth: "22rem",
-	borderRadius: "20px",
-	"@media (maxWidth: 960px)": {
-		maxWidth: "20rem",
-	},
+  maxWidth: "22rem",
+  borderRadius: "20px",
+  "@media (maxWidth: 960px)": {
+    maxWidth: "20rem",
+  },
 };
 
 export default DetailCard;
